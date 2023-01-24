fa20f845d78954a3a598235ba4090dbafb8b99b9
CuberQAQ <78301100+CuberQAQ@users.noreply.github.com>
2023-01-24 17:40:35 +0800

    Add files via upload

---
+++ page/390x450/home/index.style.js
@@ -0,0 +1,43 @@
+export const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = hmSetting.getDeviceInfo();
+
+export const TITLE_YEAR = {
+	x: px(0), // 控件相对屏幕位置
+	y: px(95), // 控件相对屏幕位置
+	w: px(480), // 控件宽度
+	h: px(60), // 控件高度,
+	align_h: hmUI.align.CENTER_H, // 水平对齐方式
+	align_v: hmUI.align.CENTER_V, // 垂直对齐方式
+	text_size: px(46), // 文字大小
+	color: 0xffffff, // 文字颜色
+};
+export const TITLE = {
+	x: px(0), // 控件相对屏幕位置
+	y: px(150), // 控件相对屏幕位置
+	w: px(480), // 控件宽度
+	h: px(50), // 控件高度,
+	align_h: hmUI.align.CENTER_H, // 水平对齐方式
+	align_v: hmUI.align.CENTER_V, // 垂直对齐方式
+	text_size: px(36), // 文字大小
+	color: 0xffffff, // 文字颜色
+};
+
+export const TIME_LINE1 = {
+	x: px(0), // 控件相对屏幕位置
+	y: px(250), // 控件相对屏幕位置
+	w: px(480), // 控件宽度
+	h: px(60), // 控件高度,
+	align_h: hmUI.align.CENTER_H, // 水平对齐方式
+	align_v: hmUI.align.CENTER_V, // 垂直对齐方式
+	text_size: px(50), // 文字大小
+	color: 0xffffff, // 文字颜色
+};
+export const TIME_LINE2 = {
+	x: px(0), // 控件相对屏幕位置
+	y: px(310), // 控件相对屏幕位置
+	w: px(480), // 控件宽度
+	h: px(60), // 控件高度,
+	align_h: hmUI.align.CENTER_H, // 水平对齐方式
+	align_v: hmUI.align.CENTER_V, // 垂直对齐方式
+	text_size: px(50), // 文字大小
+	color: 0xffffff, // 文字颜色
+};
